15006c32f9da22844c0726f1b348672ec7336e96
Peterson Fontinhas <83241372+PAFONTINHAS@users.noreply.github.com>
2025-05-29 22:54:28 -0300

    bla bla bla adicionado

---
+++ lista-participantes/criarListarParticipantes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lista.h"
+
+ListaParticipantes * criarLista(parametro, parametro, parametro, parametro){
+    bla bla bla
+    bla bla bla
+    bla bla bla
+    bla bla bla
+    bla bla bla
+    return bla bla bla ;
+
+}
