6554da30c81256d06e4255319f6bac181ec909bb
Peterson Fontinhas <83241372+PAFONTINHAS@users.noreply.github.com>
2025-06-02 21:14:22 -0300

    Alterações feitas na feature de lista-participantes

--- eventos/eventos.h
+++ eventos/eventos.h
@@ -1,7 +1,11 @@
 #ifndef EVENTOS_H
 #define EVENTOS_H
-#include "lista-participantes/lista.h"
+// #include "lista-participantes/lista.h"
 #include <stdbool.h>
+#include "eventos.h"
+#include "lista.h"
+// #include "../Trabalho-Pratico-ED1/eventos/eventos.h"
+// #include "../Trabalho-Pratico-ED1/lista-participantes/lista.h"
 
 // ESTRUTURA DE DATA E HORA PARA CADA EVENTO
 typedef struct{
@@ -15,7 +19,7 @@ typedef struct {
     char nome[100];
     DataEvento dataEvento;
     char localEvento[100];
-    ListaParticipantes inscritos; // cada evento tem uma ListaParticipantes
+    ListaParticipantes* inscritos; // cada evento tem uma ListaParticipantes
 } Evento;
 
 
@@ -36,7 +40,7 @@ typedef struct{
 GerenciadorEventos* inicializarGerenciadorEventos();
 void cadastrarNovoEvento(GerenciadorEventos* listaEventos, int codigo, const char* nome, int dia, int mes, int ano, int hora, int minuto, const char* local);
 bool validarEvento(GerenciadorEventos* listaEventos, Evento* evento);
-void buscarEvento(GerenciadorEventos* listaEventos, int codigoEvento);
+Evento* buscarEvento(GerenciadorEventos* listaEventos, int codigoEvento);
 bool cancelarEvento(GerenciadorEventos* listaEventos, int codigoEvento);
 void destruirListaEventos(GerenciadorEventos* listaEventos);
 
